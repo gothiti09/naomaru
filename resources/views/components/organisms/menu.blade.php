80ba00f4442b3df4e2ee3477b3a84272fa4e74d8
rytskywlkr <>
2022-01-09 00:18:51 +0900

    お問い合わせのリンクを修正する

--- resources/views/components/organisms/menu.blade.php
+++ resources/views/components/organisms/menu.blade.php
@@ -12,7 +12,7 @@
     <x-molecules.menu-link href="{{ route('audit.index') }}" label="監査登録" :select="request()->is('audit*')">
         <x-atoms.svg-document viewBox="24" class="text-gray-300" />
     </x-molecules.menu-link>
-    <x-molecules.menu-link href="" target="_blank" label="お問い合わせ">
+    <x-molecules.menu-link href="https://naomaru.org/contact/" target="_blank" label="お問い合わせ">
         <x-atoms.svg-question viewBox="24" class="text-gray-300" />
     </x-molecules.menu-link>
 @endif
